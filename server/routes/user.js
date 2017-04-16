dbfdc86c3c4c380e93513add0f944d58e8596dcf
Tony Bogun <antonybogun@gmail.com>
2017-04-16 19:15:19 -0400

    Add password update functionality

--- server/routes/user.js
+++ server/routes/user.js
@@ -11,7 +11,7 @@ let User = UserModel.User; // alias for User
 /* GET /login - render the login view */
 router.get('/login', (req, res, next) => {
   // check to see  if the user is not already logged index
-  if(!req.user) {
+  if (!req.user) {
     // render the login page
     res.render('user/login', {
       title: 'Login',
@@ -32,9 +32,9 @@ router.post('/login', passport.authenticate('local', {
 }));
 
 // GET /register - render the register page
-router.get('/register', (req, res, next) =>{
+router.get('/register', (req, res, next) => {
   // check if the user is not already logged in
-  if(!req.user) {
+  if (!req.user) {
     // render the registration page
     res.render('user/register', {
       title: 'Register',
@@ -49,33 +49,104 @@ router.get('/register', (req, res, next) =>{
 
 // POST /register - process the registration view
 router.post('/register', (req, res, next) => {
+  User.setPassword()
   User.register(
     new User({
-        username: req.body.username,
-        email: req.body.email,
-      }),
-      req.body.password,
-      (err) => {
-        if(err) {
-          console.log('Error insterting new user');
-          if(err.name == 'UserExistsError') {
-            req.flash('registerMessage', 'Registration Error: User Already Exists!');
+      username: req.body.username,
+      email: req.body.email,
+    }),
+    req.body.password,
+    (err) => {
+      if (err) {
+        console.log('Error insterting new user');
+        if (err.name == 'UserExistsError') {
+          req.flash('registerMessage', 'Registration Error: User Already Exists!');
+        }
+        return res.render('user/register', {
+          title: 'Register',
+          messages: req.flash('registerMessage'),
+          username: req.user ? req.user.username : ''
+        });
+      }
+      // if registration is successful
+      return passport.authenticate('local')(req, res, () => {
+        res.redirect('/');
+      });
+    });
+});
+
+/* GET /reset - render the reset password view */
+router.get('/reset', (req, res, next) => {
+  // check to see  if the user is not already logged index
+  if (req.user) {
+    // render the login page
+    res.render('user/reset', {
+      title: 'Reset',
+      messages: req.flash('error'),
+      username: req.user ? req.user.username : ''
+    });
+    return;
+  } else {
+    return res.redirect('/');
+  }
+});
+
+// POST /reset - update user's password
+router.post('/reset', (req, res, next) => {
+  console.log("USER" + req.user);
+  if (req.user) {
+    if (req.body.password == req.body.confPassword) {
+      User.findByUsername(req.body.username).then(function (sanitizedUser) {
+        if (sanitizedUser) {
+          if (sanitizedUser.email === req.body.email) {
+            sanitizedUser.setPassword(req.body.password, function () {
+              sanitizedUser.save();
+              req.flash('resetMessage', 'Password is updated successfuly!');
+              return res.render('user/reset', {
+                title: 'Reset',
+                messages: req.flash('resetMessage'),
+                username: req.user ? req.user.username : ''
+              });
+            });
+          } else {
+            req.flash('resetMessage', 'Email doesn\'t match, please try again!');
+            return res.render('user/reset', {
+              title: 'Reset',
+              messages: req.flash('resetMessage'),
+              username: req.user ? req.user.username : ''
+            });
           }
-          return res.render('user/register', {
-            title: 'Register',
+        } else {
+          req.flash('registerMessage', 'Username is not found, please try again!');
+          return res.render('user/reset', {
+            title: 'Reset',
             messages: req.flash('registerMessage'),
             username: req.user ? req.user.username : ''
           });
         }
-        // if registration is successful
-        return passport.authenticate('local')(req, res, ()=>{
-          res.redirect('/');
+      }, function (err) {
+        req.flash('registerMessage', 'Username is not found, please try again!');
+        return res.render('user/reset', {
+          title: 'Reset',
+          messages: req.flash('registerMessage'),
+          username: req.user ? req.user.username : ''
         });
+      })
+    } else {
+      req.flash('resetMessage', 'Passwords don\'t match!');
+      return res.render('user/reset', {
+        title: 'Reset',
+        messages: req.flash('resetMessage'),
+        username: req.user ? req.user.username : ''
       });
+    }
+  } else {
+    return res.redirect('/');
+  }
 });
 
 // GET /logout - logout the user and redirect to the home page
-router.get('/logout', (req, res, next)=>{
+router.get('/logout', (req, res, next) => {
   req.logout();
   res.redirect('/'); // redirect to homepage
 });
